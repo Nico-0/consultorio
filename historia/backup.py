cc3ac4da910e9c80358a7a005e2d51270164beb5
Nico <31677657+Nico-0@users.noreply.github.com>
2025-07-14 15:08:38 -0300

    OAuth y comprimir

--- historia/backup.py
+++ historia/backup.py
@@ -1,7 +1,12 @@
 #pip install google-api-python-client
 from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
 from googleapiclient.errors import HttpError
 from google.oauth2 import service_account
+
+#pip install pydrive
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 from django.conf import settings
 
 import os, sys
@@ -13,28 +18,48 @@ import shutil
 SCOPES = ['https://www.googleapis.com/auth/drive']
 SERVICE_ACCOUNT_FILE = 'service_account.json'
 filename = "db.sqlite3"
+mimeType = 'application/x-7z-compressed'
 BACKUP_TIMESTAMP = './last_backup.txt'
 
 def authenticate():
     creds = service_account.Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
     return creds
 
-def upload_file():
+def upload_file_service_account():
     creds = authenticate()
     service = build('drive', 'v3', credentials=creds)
 
     file_metadata = {
         'name' : get_nameTime(),
-        'parents' : [settings.DRIVE_FOLDER_ID]
+        'parents' : [settings.DRIVE_FOLDER_ID],
+        'mimeType': mimeType
     }
-
+    
+    media_body = MediaFileUpload(filename, mimetype=mimeType)
     file = service.files().create(
         body=file_metadata, # nombre y carpeta
-        media_body=filename # archivo a subir
+        media_body=media_body # archivo a subir
     ).execute()
 
+def upload_file(filepath):
+    gauth = GoogleAuth()
+    gauth.LocalWebserverAuth()
+    drive = GoogleDrive(gauth)
+
+    file_metadata = {
+        'title': os.path.basename(filepath),
+        'parents': [{'id': settings.DRIVE_FOLDER_ID}]
+    }
+
+    gfile = drive.CreateFile(file_metadata)
+    gfile.SetContentFile(filepath)
+    gfile.Upload()
+
+    print(f"Google Drive File ID: {gfile['id']}")
+
+
 def get_nameTime():
-    return timezone.now().strftime("%Y-%m-%d %H:%M:%S ").replace(":", ".")+filename
+    return timezone.localtime(timezone.now()).strftime("%Y-%m-%d %H:%M:%S ").replace(":", ".")+filename
 
 def get_last_backup_time():
     try:
@@ -48,7 +73,7 @@ def get_last_backup_time():
 
 def rutinaBackup():
     last_backup = get_last_backup_time()
-    delta = relativedelta(timezone.now().replace(tzinfo=None), last_backup).days
+    delta = relativedelta(timezone.localtime(timezone.now()).replace(tzinfo=None), last_backup).days
     print("\nUltimo backup: ", last_backup, file=sys.stderr)
     if(delta > 6 or last_backup is None):
         hacerBackup()
@@ -60,14 +85,14 @@ def hacerBackup():
 
     try:
         os.makedirs('./backups/', exist_ok=True)
-        result = shutil.copy(filename, './backups/'+get_nameTime())
-        print("Nuevo backup local: ", result, file=sys.stderr)
+        compressed = shutil.make_archive('./backups/'+get_nameTime(), "zip", ".", filename)
+        print("Nuevo backup local: ", compressed, file=sys.stderr)
     except FileNotFoundError:
         backupCompleto = False
     
     # online
     try:
-        upload_file()
+        upload_file(compressed)
     except FileNotFoundError:
         backupCompleto = False
         print("Falta la service account key", file=sys.stderr)
@@ -75,13 +100,14 @@ def hacerBackup():
         backupCompleto = False
         #print(err.resp, file=sys.stderr)
         print(err.content, file=sys.stderr)
+    # except ServerNotFoundError: ipconfig /flushdns
 
 
     # si se completaron backup local + online, registrar como backup realizado
     # falta considerar, si falla uno que no se haga el otro siempre
     if(backupCompleto):
         with open(BACKUP_TIMESTAMP, 'w') as f:
-            f.write(str(timezone.now().strftime("%Y-%m-%d %H:%M:%S")))
+            f.write(str(timezone.localtime(timezone.now()).strftime("%Y-%m-%d %H:%M:%S")))
         print("Backup completo\n", file=sys.stderr)
     else:
         print("Error de backup\n", file=sys.stderr)
--- readme.md
+++ readme.md
@@ -4,11 +4,23 @@
 
 - Disable debug mode in `consultorio/settings.py`
 
+- pip install django
 - `python manage.py runserver`
 - `python manage.py makemigrations`
 - `python manage.py migrate`
 
 ## Configure backups
+
+- `pip install pydrive`
+
+- In any Google Cloud project [configure Auth Platform](https://console.cloud.google.com/auth/branding). It's needed even for a local app of a single user.
+- [Enable Google Drive API](https://console.cloud.google.com/marketplace/product/google/drive.googleapis.com) and [create OAuth credentials](https://console.cloud.google.com/apis/credentials)
+- Set authorized origins to `http://localhost:8080` and authorized redirect to `http://localhost:8080/`
+- Download and place `client_secrets.json` at the root of this server.
+- [Create a new test user](https://console.cloud.google.com/auth/audience) with your desired email.
+- Copy the ID of any Drive folder from the URL, and place it in `DRIVE_FOLDER_ID` inside `consultorio/settings.py`.
+
+### (Deprecated) Configure backups with service account (missing shared drive)
 
 - `pip install google-api-python-client`
 
@@ -16,3 +28,5 @@
 - Place `service_account.json` at the root of this server.
 - In any Google Drive folder, share editor permissions with the service account's email.
 - Copy the ID of the folder from the URL, and place it in `DRIVE_FOLDER_ID` inside `consultorio/settings.py`.
+
+
